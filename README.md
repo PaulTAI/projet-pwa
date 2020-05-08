23795da9d63b29d8a163765c71314e12c5793412
lucasestrade <43579718+lucasestrade@users.noreply.github.com>
2020-05-08 21:21:03 +0200

    init

--- README.md
+++ README.md
@@ -1,9 +1,3 @@
-PROBLEME SERVICE WORKER
-
-Lorsque l'utilisateur est offline et que l'appli est déja chargé l'application fonctionne (ajout grace a idb et push sur json-server lors de la reconnexion).
-Malheureusement je n'ai pas réussi à faire fonctionner le service worker pour mettre en cache les script qui permettent l'utilisation de l'appli en offline (lors du rechargement de la page)
-Il y a mon service worker de test sous le nom "service-worker-test.js". Mais je ne comprend pas ou est l 'erreur. (décommenté le script sur index.html)
-
 Collaboration :
 - Théo Faure
 - Paul Taitinger
--- service-worker.js
+++ service-worker.js
@@ -31,7 +31,32 @@ self.addEventListener('install', function(event) {
   );
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener('fetch', function (event) {
+  const url = new URL(event.request.url);
+  const link = `${url.origin}${url.pathname}`;
+
+  if (event.request.method === 'GET') {
+    event.respondWith(
+      caches.match(event.request)
+        .then(function (response) {
+          return response || fetch(event.request)
+            .then(function (response) {
+              const responseClone = response.clone();
+              caches.open(cacheVersion)
+                .then(function (cache) {
+                  cache.put(event.request, responseClone);
+                });
+
+              return response;
+            })
+        })
+        .catch(function () {
+          return caches.match('index.html');
+        })
+    )
+  }
+});
+/*self.addEventListener('activate', event => {
   event.waitUntil(clients.claim());
 });
 
@@ -51,7 +76,7 @@ self.addEventListener('fetch', function(event) {
         })
     )
   }
-});
+});*/
 
 /*self.addEventListener('message', function(event) {
   if (event.data.type === 'SKIP_WAITING') {
